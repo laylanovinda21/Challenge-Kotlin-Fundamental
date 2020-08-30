7492adef9e5a94c2438b534d8030f7d808cb32e0
laylanovinda21 <68983100+laylanovinda21@users.noreply.github.com>
2020-08-30 21:03:35 +0700

    Add files via upload

---
+++ Challenge.kt
@@ -0,0 +1,90 @@
+import java.awt.SystemColor.menu
+import java.util.Scanner;
+
+public var input = Scanner(System.`in`)
+
+fun main() {
+    //membuat objek scanner
+    val input = Scanner(System.`in`)
+
+    //Menginputkan Identitas
+    print("Nama: ")
+    val nama: String? = input.nextLine()
+    if(nama.isNullOrEmpty()){
+
+    }
+    print("Kelas: ")
+    val kelas: String? = input.nextLine()
+    if(kelas.isNullOrEmpty()){
+
+    }
+    print("Absen: ")
+    val absen: String? = input.nextLine()
+
+    println("=========================");
+    println("Kamu ingin menghitung apa?")
+    println("1. Volume Balok\t\t\t")
+    println("2. Volume Bola\t\t\t")
+    println("0. Batal")
+    println("=========================");
+    print("Pilihanmu : ")
+    val menu = readLine()
+
+    when (menu) {
+        "1" -> {
+            println(" ")
+            println("Volume Balok")
+            print("Masukan Panjang Balok : ")
+            val p: Double = input.nextDouble()
+            print("Masukan Lebar Balok   : ")
+            val l: Double = input.nextDouble()
+            print("Masukan Tinggi Balok  : ")
+            val t: Double = input.nextDouble()
+
+            //menghitung Balok
+            val vt: Double = p * l * t;
+
+            //cetak hasil
+            println(" ")
+            println("Jadi Volume Balok= $vt ")
+
+            //kembali ke menu
+            print("Kembali ke menu [Y/N] : ")
+            val next2 = readLine()
+            if(next2=="Y" || next2=="y"){
+                main()
+            }
+            else{
+                print("Press Enter Key To Exit")
+            }
+
+        }
+        "2" -> {
+            println(" ")
+            println("Volume Bola")
+            print("Masukan jari-jari Bola : ")
+            val r: Double = input.nextDouble()
+
+            //menghitung Bola
+            val vb: Double = 4/3 * Math.PI * r * r * r;
+
+            //cetak hasil
+            println(" ")
+            println("Jadi Volume Bola= $vb ")
+
+            //kembali ke menu
+            print("Kembali ke menu [Y/N] : ")
+            val next1 = readLine()
+            if(next1=="Y" || next1=="y"){
+                main()
+            }
+            else{
+                print("Press Enter Key To Exit")
+            }
+
+        }
+        else-> {
+            println("Menu Tidak Tersedia")
+        }
+    }
+}
